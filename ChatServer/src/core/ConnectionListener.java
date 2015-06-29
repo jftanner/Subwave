96461a2141531aa484e3b7d31364460d807d27a3
James Tanner <james.tanner@tanndev.com>
2015-06-29 12:55:24 -0400

    Initial comments for ConnectionListener and SocketListener classes.

--- ChatServer/src/core/ConnectionListener.java
+++ ChatServer/src/core/ConnectionListener.java
@@ -4,22 +4,42 @@ import com.tanndev.subwave.common.Connection;
 import com.tanndev.subwave.common.Message;
 
 /**
- * Created by James Tanner on 6/28/2015.
+ * Thread class that listens for messages on a {@link com.tanndev.subwave.common.Connection}.
+ *
+ * @author James Tanner
  */
 public class ConnectionListener extends Thread {
 
-   private Connection connection;
+    /** Connection to listen for messages on */
+    private Connection connection;
 
-   public ConnectionListener(Connection connection) {
-      this.connection = connection;
-   }
+    /**
+     * Constructor
+     *
+     * @param connection connection to listen for messages on
+     */
+    public ConnectionListener(Connection connection) {
+        this.connection = connection;
+    }
 
-   public void run() {
-      while (!connection.isClosed()) {
-         Message message = connection.receive();
-         if (message == null) continue;
-         Server.sortClientMessage(connection, message);
-      }
-      Server.removeClient(connection.getClientID());
-   }
+    /**
+     * Executes on thread start.
+     * <p/>
+     * Listens for messages on the connection so long as the connection remains open. When messages are received, they
+     * are processed {@link com.tanndev.subwave.server.core.Server} class.
+     * <p/>
+     * Once the connection is closed, the associated client is shut down.
+     *
+     * @see com.tanndev.subwave.server.core.Server#sortClientMessage(com.tanndev.subwave.common.Connection, com.tanndev.subwave.common.Message)
+     * @see com.tanndev.subwave.server.core.Server#removeClient(int)
+     */
+    @Override
+    public void run() {
+        while (!connection.isClosed()) {
+            Message message = connection.receive();
+            if (message == null) continue;
+            Server.sortClientMessage(connection, message);
+        }
+        Server.removeClient(connection.getClientID());
+    }
 }
