5879ffe27874580424bee943f80279c82964ec7e
James Tanner <james.tanner@tanndev.com>
2015-06-29 11:29:25 -0400

    Initial comments for ClientRecord class.

--- ChatServer/src/core/ClientRecord.java
+++ ChatServer/src/core/ClientRecord.java
@@ -3,30 +3,66 @@ package com.tanndev.subwave.server.core;
 import com.tanndev.subwave.common.Connection;
 
 /**
- * Created by James Tanner on 6/28/2015.
+ * Instances of this class represent a single client connected to the server and store relevant information about that
+ * client.
+ *
+ * @author James Tanner
  */
 public class ClientRecord implements Comparable<ClientRecord> {
-   public final int clientID;
-   public final Connection clientConnection;
-   private String nickname;
 
-   public ClientRecord(int clientID, Connection clientConnection, String nickname) {
-      this.clientID = clientID;
-      this.clientConnection = clientConnection;
-      this.nickname = nickname;
-   }
+    /** Unique ID of the connected client. */
+    public final int clientID;
 
-   public String getNickname() {
-      return nickname;
-   }
+    /** {@link com.tanndev.subwave.common.Connection} used to communicate with the client. */
+    // TODO Encapuslate the connection object for reconnection feature.
+    public final Connection clientConnection;
 
-   public void setNickname(String nickname) {
-      this.nickname = nickname;
-   }
+    /** Friendly name used to represent the client to users. */
+    private String nickname;
 
-   @Override
-   public int compareTo(ClientRecord o) {
-      if (o == null) return 1;
-      return this.clientID - o.clientID;
-   }
+    /**
+     * Constructor
+     *
+     * @param clientID         unique ID to be used by this client
+     * @param clientConnection connection to be used for this client
+     * @param nickname         friendly name to be displayed to users
+     */
+    public ClientRecord(int clientID, Connection clientConnection, String nickname) {
+        this.clientID = clientID;
+        this.clientConnection = clientConnection;
+        this.nickname = nickname;
+    }
+
+    /**
+     * Returns the friendly name of this client.
+     *
+     * @return {@link #nickname}
+     */
+    public String getNickname() {
+        return nickname;
+    }
+
+    /**
+     * Sets the friendly name of this client.
+     *
+     * @param nickname value to assign to {@link #nickname}
+     */
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    /**
+     * Compares to another client record, using clientIDs.
+     *
+     * @param o other ClientRecord to compare against.
+     *
+     * @return comparison between clientIDs.
+     *
+     * @see java.lang.Comparable#compareTo(Object)
+     */
+    @Override
+    public int compareTo(ClientRecord o) {
+        if (o == null) return 1;
+        return this.clientID - o.clientID;
+    }
 }
