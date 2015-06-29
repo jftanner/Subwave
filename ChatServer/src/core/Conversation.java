7ddcb41609be604c8b5167ed87e876cda17cf777
James Tanner <james.tanner@tanndev.com>
2015-06-29 15:38:29 -0400

    Initial comments for ConnectionListener and SocketListener classes. (Part 1)

--- ChatServer/src/core/Conversation.java
+++ ChatServer/src/core/Conversation.java
@@ -7,86 +7,124 @@ import java.util.HashSet;
 import java.util.Iterator;
 
 /**
- * Created by James Tanner on 6/28/2015.
+ * Instances of this class represent a single conversation on the server and store relevant information about that
+ * conversation.
+ *
+ * @author James Tanner
  */
 public class Conversation {
 
-   public final int conversationID;
-   private String name;
-   private HashSet<ClientRecord> members;
+    /**
+     * Unique ID of the conversation This MUST be unique and should be generated using {@link Server#getUniqueID()}
+     */
+    public final int conversationID;
 
-   public Conversation(int conversationID, String name) {
-      this.conversationID = conversationID;
-      this.name = name;
-      members = new HashSet<ClientRecord>();
-   }
+    /**
+     * Current friendly name of the conversation, to be displayed to users.
+     *
+     * @see #getName()
+     * @see #setName(String)
+     * @see #getNameUpdateMessage()
+     */
+    private String name;
 
-   public synchronized boolean addMember(ClientRecord client) {
-      boolean result = members.add(client);
+    /** Set of all clients participating in the conversation. */
+    private HashSet<ClientRecord> members;
 
-      // Announce to all members if the add succeeded.
-      if (result) {
-         Message reply = new Message(MessageType.CONVERSATION_JOIN, conversationID, client.clientID, client.getNickname());
-         broadcastToConversation(reply);
-      }
-      return result;
-   }
+    /**
+     * Constructor
+     *
+     * @param conversationID {@link #conversationID unique ID} of the conversation
+     * @param name           {@link #name friendly name} for displaying to users
+     */
+    public Conversation(int conversationID, String name) {
+        this.conversationID = conversationID;
+        this.name = name;
+        members = new HashSet<ClientRecord>();
+    }
 
-   public synchronized boolean removeMember(ClientRecord client) {
-      boolean result = members.remove(client);
-      if (members.size() < 1) Server.removeConversation(conversationID);
-      return result;
-   }
+    /**
+     * Add a new member to the conversation. If client is successfully added, all other members are notified with a
+     * CONVERSATION_JOIN message and the new client's friendly name.
+     *
+     * @param client new member to add to the conversation
+     *
+     * @return true if the client is added, otherwise false
+     */
+    public synchronized boolean addMember(ClientRecord client) {
+        boolean result = members.add(client);
 
-   public synchronized void removeDisconnected() {
-      Iterator<ClientRecord> iterator = members.iterator();
-      while (iterator.hasNext()) {
-         ClientRecord client = iterator.next();
-         if (client.clientConnection.isClosed()) {
-            iterator.remove();
-            Message notification =
-                  new Message(MessageType.CONVERSATION_LEAVE, conversationID, client.clientID, Message.DISCONNECT_UNEXPECTED);
-            broadcastToConversation(notification);
-         }
-      }
+        // Announce to all members if the add succeeded.
+        if (result) {
+            Message reply = new Message(MessageType.CONVERSATION_JOIN, conversationID, client.clientID, client.getNickname());
+            broadcastToConversation(reply);
+        }
+        return result;
+    }
 
-   }
+    /**
+     * Remove a member from the conversation. If client is successfully added, all other members are notified with a
+     * CONVERSATION_JOIN message and the new client's friendly name.
+     *
+     * @param client new member to add to the conversation
+     *
+     * @return true if the client is added, otherwise false
+     */
+    public synchronized boolean removeMember(ClientRecord client) {
+        boolean result = members.remove(client);
+        if (members.size() < 1) Server.removeConversation(conversationID);
+        // TODO notify all members that the member left
+        return result;
+    }
 
-   public synchronized boolean broadcastToConversation(Message message) {
-      if (message.conversationID != conversationID) {
-         System.err.println("Attempted to broadcast message to a conversation using the wrong conversation ID.");
-         return false;
-      }
+    public synchronized void removeDisconnected() {
+        Iterator<ClientRecord> iterator = members.iterator();
+        while (iterator.hasNext()) {
+            ClientRecord client = iterator.next();
+            if (client.clientConnection.isClosed()) {
+                iterator.remove();
+                Message notification = new Message(MessageType.CONVERSATION_LEAVE, conversationID, client.clientID, Message.DISCONNECT_UNEXPECTED);
+                broadcastToConversation(notification);
+            }
+        }
 
-      boolean auditNeeded = false;
-      for (ClientRecord client : members) {
-         if (!client.clientConnection.send(message)) auditNeeded = true;
-      }
+    }
 
-      if (auditNeeded) removeDisconnected();
+    public synchronized boolean broadcastToConversation(Message message) {
+        if (message.conversationID != conversationID) {
+            System.err.println("Attempted to broadcast message to a conversation using the wrong conversation ID.");
+            return false;
+        }
 
-      return true;
-   }
+        boolean auditNeeded = false;
+        for (ClientRecord client : members) {
+            if (!client.clientConnection.send(message)) auditNeeded = true;
+        }
 
-   public void broadcastConversationName() {
-      broadcastToConversation(getNameUpdateMessage());
-   }
+        if (auditNeeded) removeDisconnected();
 
-   public Message getNameUpdateMessage() {
-      return new Message(MessageType.NAME_UPDATE, conversationID, 0, name);
-   }
+        return true;
+    }
 
-   public boolean hasMembers() {
-      return members.size() > 0;
-   }
+    public void broadcastConversationName() {
+        broadcastToConversation(getNameUpdateMessage());
+    }
 
-   public String getName() {
-      return name;
-   }
+    public Message getNameUpdateMessage() {
+        return new Message(MessageType.NAME_UPDATE, conversationID, 0, name);
+    }
 
-   public void setName(String name) {
-      this.name = name;
-   }
+    public boolean hasMembers() {
+        return members.size() > 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 
 }
