a24d67fcdeb06d4622b832fb77952cb5d24f43b9
james.tanner@tanndev.com <james.tanner@tanndev.com>
2015-07-05 12:56:57 -0400

    Removed unnecessary ACK from handshake

--- Common/src/Message.java
+++ Common/src/Message.java
@@ -24,9 +24,7 @@ public class Message implements Serializable {
 
    // Default Messages:
    public static final String CONNECTION_START_ACK = "Connection request received.";
-   public static final String CONNECTION_FINAL_ACK = "Connection accepted.";
    public static final String DISCONNECT_INTENT = "Goodbye.";
-   public static final String DISCONNECT_UNEXPECTED = "Disconnected.";
    public static final String UNHANDLED_MSG = "Not configured to handle that message type.";
    public static final String CRITICAL_ERROR = "A critical error has occurred. Shutting down.";
    public static final String INVALID_SOURCE_ID = "ClientID does not match ID of source.";
